25b88a01cdf7fa4307eaeb6e3cd1af8987f21b42
Jose <oladotjos@gmail.com>
2025-01-24 12:23:46 +0100

    update-3

--- server.js
+++ server.js
@@ -13,6 +13,11 @@ const io = new Server(server,{
     }});
 
     app.use(cors());
+    app.use(express.static('public')); 
+
+    app.get('/', (req, res) => {
+        res.send('Welcome to the TikTok Verification App!');
+    });
 
 // const tiktokUsername = 'ac_grosshandel';
 const tiktokUsername = 'kitty_the_queen';
