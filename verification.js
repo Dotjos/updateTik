1bd4ca10a33aab5fc66b847543cd4270928d8cf0
Jose <oladotjos@gmail.com>
2025-01-29 13:28:25 +0100

    dododo

--- verification.js
+++ verification.js
@@ -3,9 +3,9 @@ const loadingText = document.querySelector(".load");
 const clearButton = document.querySelector(".clearBtn");
 
 // const link= io('https://tiktoknummer.de'|| "https://updatetik.onrender.com/"); // Adjust to your server
-const socket = io(location.hostname === "tiktoknummer.de" ? "https://tiktoknummer.de" : "https://updatetik.onrender.com/");
+// const socket = io(location.hostname === "tiktoknummer.de" ? "https://tiktoknummer.de" : "https://updatetik.onrender.com/");
 
-// const socket = io('https://tiktoknummer.de'); // Adjust to your server
+const socket = io('https://updatetik.onrender.com/'); // Adjust to your server
 import { getAllOrders, verifyBidder } from "./wooCommerce.js";
 
 document.addEventListener('DOMContentLoaded', loadStoredComments);
