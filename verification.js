eeae793e3fb53c0189bd69d3c45239d95ec68141
Jose <oladotjos@gmail.com>
2025-01-29 13:25:35 +0100

    ahhhh

--- verification.js
+++ verification.js
@@ -3,7 +3,7 @@ const loadingText = document.querySelector(".load");
 const clearButton = document.querySelector(".clearBtn");
 
 // const link= io('https://tiktoknummer.de'|| "https://updatetik.onrender.com/"); // Adjust to your server
-const link = io(location.hostname === "tiktoknummer.de" ? "https://tiktoknummer.de" : "https://updatetik.onrender.com/");
+const socket = io(location.hostname === "tiktoknummer.de" ? "https://tiktoknummer.de" : "https://updatetik.onrender.com/");
 
 // const socket = io('https://tiktoknummer.de'); // Adjust to your server
 import { getAllOrders, verifyBidder } from "./wooCommerce.js";
