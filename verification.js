9f7f648e50136cf3eebddf70e24fe5325dadbf79
Jose <oladotjos@gmail.com>
2025-02-08 17:41:24 +0100

    hghgghg

--- verification.js
+++ verification.js
@@ -44,7 +44,7 @@ async function loadStoredComments() {
         }
     }, 60000); 
 //switching off previous instances
-    // socket.off('chat-message');
+    socket.off('chat-message');
 
     // ✅ Listen for new messages
     socket.on('chat-message', async (messageData) => {
