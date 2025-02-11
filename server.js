5a0759bdc9d0b31c9085840ba01b44066e617a81
Jose <oladotjos@gmail.com>
2025-02-11 11:06:55 +0100

    ndhh,

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const path = require('path')
 
 const io = new Server(server,{
     cors: {
-        origin: ['http://localhost:3000', 'https://updatetik.onrender.com',"https://updatetik-t9b6.onrender.com/"],
+        origin: ['http://localhost:3000',"https://updatetik-t9b6.onrender.com/"],
         methods: ["GET", "POST"]
     }});
 
